0486c17e9e58f73bfe1b7ba7f729b524e6e326fc
Serhii Ostapenko <ostapkinpaskal@gmail.com>
2023-04-23 23:50:57 +0300

    add localStorage logic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,6 +17,19 @@ class App extends React.Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem('contacts'));
+    if (contacts) {
+      this.setState({ contacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   formSubmitHandler = (data) => {
     const { name } = data;
     if (this.isNameExist(name)) {
